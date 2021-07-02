9c1e3eb9a059da426d742d2b1004ce3b277ee97a
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2021-07-01 22:44:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navButton = document.querySelector(".navbar__nav-button");
+
+navButton.addEventListener("click", function(e) {
+    const navList = document.querySelector(".navbar__nav-list");
+
+    navList.classList.toggle("navbar__nav-list--open");
+});
+
