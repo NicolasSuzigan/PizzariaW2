fe569b74193b334948e6b5eb195007a5b61fdc98
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2021-07-02 23:27:11 -0300

    Iniciando backand e PHP KKK Tentando

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  
+  $valor1 = $_GET["Abobrinha"];
+  $valor2 = $_GET["Bacon"];
+  $valor3 = $_GET["name"];
+
+  echo " o pedido do".$valor3. " o valor da Abobrinha eh:" .$valor1. "bacon" .$valor2.;
+?>
